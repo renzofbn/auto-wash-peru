1f19e7422dab3a391508befc92061db6ae5952a9
Renzo <renzofbn@tutanota.com>
2022-11-23 18:27:59 -0500

    MERGE

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
-    <title>Document</title>
-</head>
-<body>
-    <a href="{{route('servicio.index')}}" class="btn btn-info">Servicio</a>
-=======
     <title>Inicio</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
@@ -23,6 +17,5 @@
     <a href="{{ route('cliente.index') }}" class="btn btn-outline-success">
         cliente
     </a>
->>>>>>> b3b34599b35ce6e747f56f4b1ba68386e70f84cb
 </body>
 </html>
